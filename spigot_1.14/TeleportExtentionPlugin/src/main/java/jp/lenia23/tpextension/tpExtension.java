960756a8496e0fd0d96b120cc1a1a7156ab98764
lenia23 <>
2019-07-26 17:03:34 +0900

    Add teleport extension plugin

---
+++ spigot_1.14/TeleportExtentionPlugin/src/main/java/jp/lenia23/tpextension/tpExtension.java
@@ -0,0 +1,55 @@
+package jp.lenia23.tpextension;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Set;
+
+public class tpExtension extends JavaPlugin {
+    public FileConfiguration config;
+
+    @Override
+    public void onEnable() {
+        this.getCommand("tpe").setExecutor(new tpExtensionCommand(this));
+
+        this.saveDefaultConfig();
+        config = getConfig();
+        getLogger().info("TpExtensionPlugin is loaded!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("TpExtensionPlugin is disabled");
+    }
+
+    public void addLabel(String label, int x, int y, int z) {
+        this.config.set(label+".x", x);
+        this.config.set(label+".y", y);
+        this.config.set(label+".z", z);
+
+        saveConfig();
+        reloadTpConfig();
+    }
+
+    public void removeLabel(String label) {
+        this.config.set(label, null);
+        saveConfig();
+        reloadTpConfig();
+    }
+
+    public Set<String> getListLabels() {
+        return config.getKeys(false);
+    }
+    public String getLabelVal(String label, String key) {
+        return config.getString(label+'.'+key);
+    }
+
+    public void reloadTpConfig() {
+        reloadConfig();
+        config = getConfig();
+    }
+
+    public boolean exists(String label){
+        return config.contains(label);
+    }
+}
